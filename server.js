c76efe2058a97d3e161096c8e3ca01c3d8574a1c
Olaoluwa99 <olaoluwadaniel99@gmail.com>
2025-10-18 21:21:23 +0100

    Update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/me', async (req, res) => {
 });
 
 // Start the Server
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
